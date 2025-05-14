1f5001d2122c1466016feee477bfa972b341718d
ruchap24 <patilrucha991@gmail.com>
2025-05-14 17:07:40 +0530

    updated

---
+++ backend/controllers/auth.js
@@ -0,0 +1,14 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+exports.registerUser = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({ username, password: hashedPassword });
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
